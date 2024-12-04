b914b9bcd4fb6c676c4c6a63206b4dfe56de828a
LawrenciaKlu <you@example.com>
2024-12-04 11:32:13 -0800

    adding all files

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// function checkGender() {
+//         let gender = prompt("What is your gender?");
+//         if (gender == "male 👨🏼") {
+//             alert("You are a man");
+//         } else if (gender == "female") {
+//             alert("You are a girl 👩🏼");
+//         } 
+//     }
+
+    // checkGender()
+
+    function checkGender(){
+        let gender =prompt("What is your gender?");
+        gender = gender .toLowerCase();
+
+        if (gender =="female"){
+        alert(`You are a ${gender}👩🏼`)
+        } 
+        else if (gender =="male"){
+            alert(`You are a ${gender}👨🏼`)
+        } 
+        else {
+            alert(`invalid input`)
+        }
+    }
+    checkGender()
+    
